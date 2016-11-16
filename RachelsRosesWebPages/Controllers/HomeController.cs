22e1db50504e0e8e85cc9627b7874b75e904d1ac
rachelbarnes <rachelbarnesjn116@yahoo.com>
2016-11-16 18:57:53 -0500

    Making notes, adding some very basic functionality

--- RachelsRosesWebPages/Controllers/HomeController.cs
+++ RachelsRosesWebPages/Controllers/HomeController.cs
@@ -6,20 +6,35 @@ using System.Web.Mvc;
 
 namespace RachelsRosesWebPages.Controllers {
     public class HomeController : Controller {
+        public class Ingredient {
+            public string name;
+            public string measurement;
+        }
+        public static Dictionary<string, string> ingredients = new Dictionary<string, string>();
         public ActionResult Index() {
             return View();
         }
-
+        public ActionResult Delete(string item) {
+            ingredients.Remove( item);
+            return Redirect("/home/about"); 
+        }
+        public ActionResult CreateIngredient(string ingredient, string quantity) {
+            ingredients.Add(ingredient, quantity);
+            return Redirect("/home/about"); 
+        }
         public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.ingredients = ingredients;
             return View();
         }
-
         public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
 }
+
+//notes for About.cshtml: 
+//there's something wrong with lines 13-20... the value isn't being displayed and I can't delete things from    
+//the dictionary, which may mean that hte item isn't being passed correctly... 
+//look into this more... i'm not having trouble with the one Steve helped me set up, so do a comparison analysis
+
