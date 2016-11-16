302ad3781027d706bffae840d198abbdda3511c3
rachelbarnes <rachelbarnesjn116@yahoo.com>
2016-11-16 13:54:10 -0500

    First commit:

---
+++ RachelsRosesWebPages/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RachelsRosesWebPages.Startup))]
+namespace RachelsRosesWebPages
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
